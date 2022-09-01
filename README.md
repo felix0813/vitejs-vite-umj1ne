b1dfb8439d20253bb9d73fe69deea67951f09795
felix0813 <1306332027@qq.com>
2022-09-01 14:22:59 +0800

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# vitejs-vite-umj1ne
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/vitejs-vite-umj1ne)
---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,21 @@
+<script setup>
+import { ref, reactive } from 'vue';
+
+defineProps({});
+
+const list = ref([]);
+const obj = reactive({});
+const addToList = () => {
+  if (list.value.length < 5) {
+    list.value.push(Math.random().toString(36).slice(-6));
+  }
+};
+</script>
+
+<template>
+  <button @click="addToList">添加</button>
+  <button @click="list.pop()">删除</button>
+  <p v-for="item in list">
+    {{ item }}
+  </p>
+</template>
